7c086ebf1fdf59acc2fa7cb5bc912d9290958b49
Youssef Fatihi <Usefps@gmail.com>
2024-09-01 18:33:56 +0100

    for github

--- docs/_build/html/_static/custom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var tocItems = document.querySelectorAll('li.toctree-l1 > a');
-    tocItems.forEach(function(item) {
-        var parentLi = item.parentNode;
-        parentLi.classList.add('current');
-    });
-});
