fc0d501c4ee7dea2ad6dfedeacd9c99f801372e1
Youssef Fatihi <Usefps@gmail.com>
2024-09-04 16:41:57 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/runner/work/Protomix/Protomix'))
+sys.path.insert(0, os.path.abspath('../Protomix'))
 
 # import sphinx_rtd_theme
 
