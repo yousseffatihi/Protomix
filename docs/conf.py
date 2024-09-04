39f8a8e65f7b90848c7d77e39380ff9be529e1de
Youssef Fatihi <Usefps@gmail.com>
2024-09-04 16:57:42 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ copyright = '2024, Zniber Mohammed'
 author = 'Zniber Mohammed'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = ''
 
 # -- General configuration ---------------------------------------------------
 
